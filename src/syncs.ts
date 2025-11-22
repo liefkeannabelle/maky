bfedf937e381b8f07263cad53d0a0e4f22b0d06f
Yahir H <Yahirhernandez304@gmail.com>
2025-11-22 15:44:03 -0500

    fixed syncs bug?

---
+++ src/syncs.ts
@@ -0,0 +1,7 @@
+// file: src/syncs.ts
+// Bridge file so imports like `import syncs from "@syncs"` work.
+// It simply re-exports the auto-generated syncs barrel.
+
+import syncs from "./syncs/syncs.ts";
+
+export default syncs;
