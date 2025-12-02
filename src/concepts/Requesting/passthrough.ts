e4b65a7781efd7f4022d2984228de24ed183c83b
Yahir H <Yahirhernandez304@gmail.com>
2025-12-01 22:43:16 -0500

    Songs for users with 1-2 songs

--- src/concepts/Requesting/passthrough.ts
+++ src/concepts/Requesting/passthrough.ts
@@ -112,6 +112,7 @@ export const exclusions: Array<string> = [
   "/api/Song/updateSong",
   "/api/Song/_getSongCount",
   "/api/Song/_getAllSongsForRecommendation",
+  "/api/Song/_getSuggestedSongs",
   "/api/Chord/_getAllChords",
   "/api/Chord/_getChordByName",
   "/api/Chord/_getChordDiagram",
